242bbc37e904814132bc6c0dc4a8637767cd4507
instruktor <instruktor@instruktor-pc.319.wroclaw.altkom.local>
2014-07-10 14:13:35 +0200

    v001

--- src/beans/CitiesWebReader.java
+++ src/beans/CitiesWebReader.java
@@ -4,45 +4,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.Set;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CitiesWebReader {
-	
-	public Set readCities() throws IOException {
-			URL url = new URL("http://en.wikipedia.org/wiki/List_of_cities_in_Georgia%2C_USA");
-			BufferedReader in = new BufferedReader(
-						new InputStreamReader( url.openStream() ) );
-			String result = "";
-			String line;
-			while ((line = in.readLine()) != null) {
-				if ( line == null ) {
-                    continue;
-                }
-				String parsedWord = parseLine( line );
-				
-				if ( parsedWord != null ) {
-					result += parsedWord + System.getProperty( "line.separator" );
-				}
+
+	public ArrayList<String> readCities() throws IOException {
+		ArrayList<String> al = new ArrayList<String>();
+		URL url = new URL(
+				"http://en.wikipedia.org/wiki/List_of_cities_in_Georgia%2C_USA");
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				url.openStream()));
+		String line;
+		while ((line = in.readLine()) != null) {
+			String parsedWord = parseLine(line);
+
+			if (parsedWord != null) {
+				al.add(parsedWord);
 			}
-			in.close();
-			
-			return result;
+		}
+		in.close();
+
+		return al;
 	}
 
 	private String parseLine(String line) {
-	       if ( line != null ) {
-	            Pattern pattern = Pattern.compile( "<li><a href=.*title=\".*,.*\">(.*)</a></li>" );
-	            Matcher matcher = pattern.matcher( line );
-	            boolean matchesPattern = matcher.find();
+		if (line != null) {
+			Pattern pattern = Pattern
+					.compile("<li><a href=.*title=\".*,.*\">(.*)</a></li>");
+			Matcher matcher = pattern.matcher(line);
+			boolean matchesPattern = matcher.find();
+			if (matchesPattern) {
+				return matcher.group(matcher.groupCount());
+			}
+		}
 
-	            if (matchesPattern) {
-	                return matcher.group( matcher.groupCount() );
-	            } 
-	        }
+		return null;
+	}
 
-	        return null; 
-	 }
-	
 }
